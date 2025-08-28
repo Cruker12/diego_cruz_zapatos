61a84b7b60817ef219cdf3a11d4e299dc8fc87a5
Nicolas <nicolasrestrepo1143183@correo.itm.edu.co>
2025-08-28 09:23:10 -0500

    Commit rama

--- Proyect_Zapatos_Parcial/Program.cs
+++ Proyect_Zapatos_Parcial/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class Zapatos
+{
+    public int Id;
+    public string? Nombre;
+    public string? Color_Zapato;
+    public decimal Medida_Pie;
+    public Ventas? Ventas;
+    public List<Cordones>? Cordones;
+    public List<Tipos>? Tipos;
+}
+
+public class Cordones
+{
+    public int Id;
+    public string? Color_Cordon;
+    public decimal Cant_Metros;
+}
+
+public class Tipos
+{
+    public int Id;
+    public string? Nombre_Tipo;
+}
+
+public class Ventas
+{
+    public int Id;
+    public string? Tipo_Venta;
+
+}
+
